fd3ee40f62c71812280acbddfcfe64314d822608
pranik chainani <pranikc@gmail.com>
2021-08-13 09:52:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    
+}
+
+function setBestCustomer(){
+    bestCustomer = "not" + " " + customerName.toLowerCase()
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe" + " " + customerName.toLowerCase()
+}
+
+const leastFavCust = "Louis"
+function changeLeastFavoriteCustomer(){
+    leastFavCust = "Rachel"
+}
